64e8474139898789d6ebfc2ad1ab739bcf6c355a
Mir47-47 <53306904+Mir47-47@users.noreply.github.com>
2025-07-07 15:11:41 +0900

    beakjun10814

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import silver.bj10814;
+
+public class Main {
+    public static void main(String[] args) {
+        bj10814 solution = new bj10814();
+        solution.bj10814();
+    }
+}
---
+++ src/silver/bj10814.java
@@ -0,0 +1,40 @@
+package silver;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class bj10814 {
+    public void bj10814() {
+        int N = 0;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        Person[] person = new Person[N];
+        for (int i = 0; i < N; i++) {
+            person[i] = new Person(sc.nextInt(), sc.next(), i);
+        }
+
+
+        Arrays.sort(person, Comparator.comparing((Person p) -> p.age)
+                .thenComparing(p -> p.order));
+
+
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(person[i].age + " "+ person[i].name+"\n");
+        }
+    }
+
+    static class Person {
+        int age;
+        String name;
+        int order;
+
+        Person(int age, String name, int order) {
+            this.age =  age;
+            this.name = name;
+            this.order = order;
+        }
+    }
+}
